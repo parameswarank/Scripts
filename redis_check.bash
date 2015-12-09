e6a62dc84820766cbf038b2a3c1b9ecf90479fb9
Parameswaran-K <parames@wfspl.com>
2015-12-09 16:21:50 +0530

    Redis Server Check
    Check whether redis running or not

---
+++ redis_check.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <port>"
+  exit
+fi
+
+PORT=$1
+
+nc 127.0.0.1 $PORT </dev/null
+if [ $? -ne 0 ]; then
+	echo "status err no server listening!"
+else
+  echo "status ok ok"
+fi
